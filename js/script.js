1b79617f624fb741e02bf9616fa5723a2ed928fa
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-13 16:31:03 +0200

    generatore password completato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let userName = prompt("Inserisci il tuo nome");
-// console.log(userName);
+// Chiedi all'utente il suo nome
+const userName = prompt("Inserisci il tuo nome");
+console.log(userName);
+document.getElementById("user-name").innerHTML = userName;
 
-document.getElementsByClassName("user-name").innerHTML = userName;
+// Chiedi all'utente il suo cognome
+const userSurname = prompt("Inserisci il tuo cognome");
+console.log(userSurname);
+document.getElementById("user-surname").innerHTML = userSurname;
+
+// Chiedi all'utente il suo colore preferito
+const userFavColor = prompt("Inserisci il tuo colore preferito");
+console.log(userFavColor);
+document.getElementById("user-favcolor").innerHTML = userFavColor;
